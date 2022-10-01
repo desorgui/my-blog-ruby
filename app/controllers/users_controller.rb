81063f2d5a11978ce829df71c2a811aa1ca9eaaf
Desor Guishny <desorguishny@gmail.com>
2022-10-01 12:01:31 -0400

    Add Views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
-    puts params
+    @users = User.all
   end
 
   def show
-    puts params
+    param_id = params['id'].to_i
+    @user_to_find = User.find_by(id: param_id)
+    @user_found_posts = Post.where(author_id: param_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[show] do
+  resources :users, only: %i[index, show] do
     resources :posts, only: %i[index, show]
   end
 
-  get "/", to: "users#index"
+  get "/", to: "users#index"  
+  get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
-
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id/posts/:id", to: "posts#show"
 
