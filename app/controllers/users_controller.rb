181ddb34e72839f51b4e166cf4ee49663e6318e2
Desor Guishny <desorguishny@gmail.com>
2022-10-06 15:17:33 -0400

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     param_id = params['id'].to_i
     # @user_posts = User.where(id:param_id).includes(:most_recent_posts).first.most_recent_posts
