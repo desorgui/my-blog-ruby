6cac7fd26e7e9b614951e2a8e8ae16d49bba0265
Desor Guishny <desorguishny@gmail.com>
2023-02-02 18:05:56 -0500

    Update single post component

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,27 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 250, too_long: '%<max> characters is the maximum allowed' }
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  # include PgSearch::Model
+  # belongs_to :user
+  # pg_search_scope :search_by_title,
+  #                 against: [
+  #                   [:title, 'A'],
+  #                   [:body, 'C']
+  #                 ],
+  #                 associated_against: {
+  #                   user: { name: 'B' }
+  #                 },
+  #                 using: {
+  #                   tsearch: { prefix: true, any_word: true },
+  #                   trigram: {
+  #                     only: [:title],
+  #                     threshold: 0.25
+  #                   }
+  #                 }
+
+  # validates :title, presence: true, length: { maximum: 100 }
+  # validates :body, presence: true, length: { minimum: 50, maximum: 5000 }
+
   after_initialize do |post|
     post.likes_counter = 0
     post.comments_counter = 0
